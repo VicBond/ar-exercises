6b18b8efe4a7ea6712d19d01f3ab9bc26ccc8f77
Victor Bondarenko <nirasimha@gmail.com>
2022-01-03 19:53:55 +0000

    Validations for both models

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,16 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+class Employees < ActiveRecord::Base
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  # validates :hourly_rate, 
+  validates :hourly_rate, numericality: { only_integer: true }, length: {maximum: 40, minimium: 200}
+  validates :store_id, presence: true
+end
+
+class Store < ActiveRecord::Base
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: {greater_than_or_equal: 0}
+end
