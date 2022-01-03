1c23071eb10e59b8f2fc36b7d0079e0254f90b7c
Victor Bondarenko <nirasimha@gmail.com>
2022-01-03 18:20:58 +0000

    statements changed

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -8,10 +8,10 @@ puts "----------"
 store1 = Store.create(name: 'Burnaby', annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
 store1.save
 
-store2 = Store.create(name: 'Richmond', annual_revenue: 1260000, mens_apparel: true, womens_apparel: true)
+store2 = Store.create(name: 'Richmond', annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
 store2.save
 
-store3 = Store.create(name: 'Gastown', annual_revenue: 190000, mens_apparel: true, womens_apparel: true)
+store3 = Store.create(name: 'Gastown', annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
 store3.save
 
 puts Store.count
